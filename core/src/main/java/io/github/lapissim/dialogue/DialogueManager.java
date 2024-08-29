6ad32802d1bf656e8ad3df9d6b4721baa44e7ed8
DogeDoge <DogeDoge17@users.noreply.github.com>
2024-08-29 13:09:18 -0500

    Update DialogueManager.java

--- core/src/main/java/io/github/lapissim/dialogue/DialogueManager.java
+++ core/src/main/java/io/github/lapissim/dialogue/DialogueManager.java
@@ -3,12 +3,13 @@ package io.github.lapissim.dialogue;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 
+import java.sql.Array;
 import java.util.Arrays;
 import java.util.List;
 
 public class DialogueManager
 {
-    public static List<String> rawLines;
+    public static String[] rawLines;
     public static String rawDialogue;
     public static int linePointer;
 
@@ -17,9 +18,10 @@ public class DialogueManager
         {
             FileHandle hi = Gdx.files.internal(path);
             rawDialogue = hi.readString();
-            rawLines = Arrays.stream(rawDialogue.split("\n")).toList();
-            for(int i = 0; i < rawLines.size();i++){
-
+            rawLines = rawDialogue.split("\n");
+            for(int i = 0; i < rawLines.length; i++)
+            {
+                
             }
 
 
